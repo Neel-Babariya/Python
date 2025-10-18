8d60fecfe61a8f249644367d3a8ad524b1e6c193
Neel-Babariya <chetanabenbabariya58@gmail.com>
2025-10-18 09:52:55 +0530

    Implement age and username validation on login
    Added username validation and adjusted sign-in logic.

--- LoginPage.py
+++ LoginPage.py
@@ -3,6 +3,16 @@ print("Login Page")
 age = int(input("enter your age here")) 
 
 if age >= 18 :
-  print("you are signed in") 
+  pass
 else :
   print("you must be 18") 
+
+name = input("enter your name here: ") 
+letters = name.count() 
+
+if letters == 0 :
+  print(" please enter a valid username: ") 
+elif letters <= 12 :
+  print (" you are signed in! ") 
+else :
+  print(" the username must be shorter than 12 characters") 
